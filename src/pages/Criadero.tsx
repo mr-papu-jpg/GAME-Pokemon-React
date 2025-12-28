a5d822a0e49f1b9aec95d4b2dacbfb7e9b569735
mr-papu-jpg <angstart.1109@gmail.com>
2025-12-28 19:39:53 -0400

    fix(criadero): corregir renderizado de lista de pokémon

--- src/pages/Criadero.tsx
+++ src/pages/Criadero.tsx
@@ -13,29 +13,30 @@ const Criadero: React.FC = () => {
         <h1>Mi Criadero</h1>
         <p>Tienes {user?.pokemonTeam.length} Pokémon en tu colección</p>
       </header>
-
       <div className="pokemon-grid">
         {user?.pokemonTeam.length === 0 ? (
-          <div className="empty-msg">
-            <p>Aún no has capturado ningún Pokémon...</p>
-            <Link to="/busqueda" className="go-search-btn">Ir a buscar</Link>
-          </div>
-        ) : (
-          user?.pokemonTeam.map((poke, index) => (
-            <div key={`${poke.id}-${index}`} className="mini-pokemon-card">
-              <img src={poke.sprites.front_default} alt={poke.name} />
-              <h3>{poke.name}</h3>
-              <div className="mini-types">
-                {poke.types.map((t: any) => (
-                  <span key={t.type.name} className={`mini-type ${t.type.name}`}>
-                    {t.type.name}
-                  </span>
-                ))}
-              </div>
+            <div className="empty-msg">
+                <p>Aún no has capturado ningún Pokémon...</p>
+                <Link to="/busqueda" className="go-search-btn">Ir a buscar</Link>
             </div>
-          ))
+        ) : (
+        /* Fíjate en el uso de (poke, index) => ( ... ) con paréntesis */
+        user?.pokemonTeam.map((poke, index) => (
+        <div key={`${poke.id}-${index}`} className="mini-pokemon-card">
+            <img src={poke.sprites.front_default} alt={poke.name} />
+            <h3 style={{ color: '#333' }}>{poke.name}</h3>
+            <div className="mini-types">
+            {poke.types.map((t: any) => (
+                <span key={t.type.name} className={`mini-type ${t.type.name}`}>
+                {t.type.name}
+                </span>
+            ))}
+            </div>
+        </div>
+        ))
         )}
-      </div>
+    </div>
+
     </div>
   );
 };
